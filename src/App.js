501db250e13659986583c159e2ebf2544183f309
Bryanna <bryanna@Bryannas-MBP.lan>
2022-11-26 15:55:25 -0500

    made progress

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem.js";
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+/* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
+bakeryData.forEach((item) => {
+  item.image = process.env.PUBLIC_URL + "/" + item.image;
+});
+/* ############################################################## */
 
 function App() {
+  // TODO: use useState to create a state variable to hold the state of the cart
+  /* add your cart state code here */
+
+  const [cart, nextCart] = useState([]);
+  const [total, nextTotal] = useState(0);
+  const [filteredItems, setFilteredItems] = useState(bakeryData);
+
+  function addToCart(item) {
+    item.count += 1;
+    if(cart.includes(item)) {
+      nextCart(cart);
+    } else {
+      nextCart([...cart, item]);
+    }
+  }
+
+  function removeFromCart(item) {
+    item.count -= 1;
+    const newCart = cart.filter((x) => x != item)
+    if(item.count === 0) {
+      nextCart(newCart);
+    } else {
+      nextCart([...newCart, item]);
+    }
+    updateTotal(item, false)
+  }
+
+  function updateTotal(item, addP) {
+    var newTotal = parseFloat(total);
+    if(addP) {
+      newTotal += parseFloat(item.price);
+    } else {
+      newTotal -= parseFloat(item.price);
+    }
+    nextTotal(newTotal.toFixed(2));
+  }
+
+  function handleClick(item) {
+    addToCart(item);
+    updateTotal(item, true);
+  }
+
+  function filterColor(color) {
+    var filtered = bakeryData.filter(item => item.color === color);
+    setFilteredItems(filtered);
+  }
+
+  function resetFilter() {
+    setFilteredItems(bakeryData);
+  }
+
+  function sortPrice() {
+    const sorted = [...filteredItems]; // copy of current items
+    sorted.sort((a, b) => (a.price - b.price)); //sort copy
+    setFilteredItems(sorted);
+  }
+
+  var componentArray = [];
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <img src="./images/logo.png"/> {/* TODO: personalize your bakery (if you want) */}
+      
+      
+      {componentArray = filteredItems.map((item) => (
+        <BakeryItem item={item} handleClick={handleClick} removeFromCart={removeFromCart}/>
+      ))}
+      
+
+      <button onClick={() => {filterColor("blue")}}>Filter blue</button>
+      <button onClick={() => {filterColor("black")}}>Filter black</button>
+      <button onClick={() => {resetFilter()}}>reset</button>
+      <button onClick={() => {sortPrice()}}>sort</button>
+
+      <div>
+        <h2>Cart</h2>
+        {cart.map((item) => (<p>{item.name + ' x' + item.count}</p>))}
+        <p>Total: {total}</p>
+      </div>
     </div>
   );
 }
